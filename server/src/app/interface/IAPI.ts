5fd6e1ebc4f7ef0f20a9847a454999ef6746735e
Javier Ramos <alu0101313313@ull.edu.es>
2025-11-12 23:13:49 +0000

    {U}: Update IApi.ts

--- server/src/app/interface/IAPI.ts
+++ server/src/app/interface/IAPI.ts
@@ -4,9 +4,6 @@ export const API_URL = `http://api.tcgdex.net/v2/${ILanguage.EN}`;
 export const API_IMAGE_URL = `https://assets.tcgdex.net/${ILanguage.EN}`;
 
 /*
-
 http://api.tcgdex.net/v2/${API_LANGUAGE}/${API_DATA}?${filter}
-
 http://assets.tcgdex.net/${API_LANGUAGE}/${card.id_card.set}/${card.id_card.number}/${quality}.png
-
 */
