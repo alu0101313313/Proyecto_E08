bc3fb5137b70b2a6be38fca24fe2ccd4e3b99ed0
Javier Ramos <alu0101313313@ull.edu.es>
2025-11-14 09:52:05 +0000

    {U}: Add GET operation with name filter

--- server/src/app/routers/serieRouter.ts
+++ server/src/app/routers/serieRouter.ts
@@ -26,9 +26,10 @@ serieRouter.post("/series", async (req, res) => {
     const serieDict = dataclassToDict(apiResponse);
     const serieJSON = JSON.stringify(serieDict, null, 2);
     const serieData = JSON.parse(serieJSON) as ISeries;
+    const newSerie = new Series(serieData);
 
-    await serieData.save();
-    res.status(201).json({ message: "Serie created successfully", serieData });
+    await newSerie.save();
+    res.status(201).json({ message: "Serie created successfully", newSerie });
 
   } catch (error: any) {
     if (error.code === 11000) {
@@ -38,11 +39,22 @@ serieRouter.post("/series", async (req, res) => {
   }
 });
 
-serieRouter.get("/series/:id", async (req, res) => {
-  const { id } = req.params;
+serieRouter.get("/series", async (req, res) => {
+  const filter = req.query.name ? { name: req.query.name.toString() } : {};
 
   try {
-    const serie = await Series.findOne({ id });
+    const series = await Series.find(filter);
+    res.status(200).json(series);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving series", error });
+  }
+
+});
+
+serieRouter.get("/series/:id", async (req, res) => {
+  try {
+    const query = req.params.id
+    const serie = await Series.findOne({ _id: query });
     if (!serie) {
       return res.status(404).json({ message: "Serie not found" });
     }
@@ -50,15 +62,15 @@ serieRouter.get("/series/:id", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Error retrieving serie", error });
   }
+
 });
 
 // serieRouter.patch() <- no se si va a hacer falta
 
 serieRouter.delete("/series/:id", async (req, res) => {
-  const { id } = req.params;
-
   try {
-    const deletedSerie = await Series.findOneAndDelete({ id });
+    const query = req.params.id
+    const deletedSerie = await Series.findOneAndDelete({ _id: query });
     if (!deletedSerie) {
       return res.status(404).json({ message: "Serie not found" });
     }
@@ -66,4 +78,4 @@ serieRouter.delete("/series/:id", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Error deleting serie", error });
   }
-}); 
+});  
