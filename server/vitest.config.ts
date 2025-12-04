a11b9348a7c5c233381c0173079e82cde76c1a95
Javier Ramos <alu0101313313@ull.edu.es>
2025-12-04 15:32:01 +0000

    {U}: 1ra prueba Coveralls

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 });
+
+
